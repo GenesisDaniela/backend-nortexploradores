122646f05d89af46987d3092245465e95c872caf
Santiago Serrano <santiagoandressp@ufps.edu.co>
2021-09-22 15:30:45 -0500

    Code with me

--- src/main/java/com/example/demo/rest/AlojamientoRest.java
+++ src/main/java/com/example/demo/rest/AlojamientoRest.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200/")
 @Slf4j
 public class AlojamientoRest {
-
+//Ola
     @Autowired
     AlojamientoService aser;
 
