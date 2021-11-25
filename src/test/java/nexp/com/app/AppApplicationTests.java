bedd488c92c238fe2b6b6cc7f22d61f182ad2bca
GenesisDaniela <genesisdanielavjau@ufps.edu.co>
2021-11-25 14:35:48 -0500

    Se agregó la clase de test y las dependencias respectivas junit VS 4.12

--- src/test/java/nexp/com/app/AppApplicationTests.java
+++ src/test/java/nexp/com/app/AppApplicationTests.java
@@ -3,16 +3,19 @@ package nexp.com.app;
 import lombok.extern.slf4j.Slf4j;
 import nexp.com.app.model.Paquete;
 import nexp.com.app.service.PaqueteService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Service;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@Service
-@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
 class AppApplicationTests {
 
 	@Autowired
@@ -20,10 +23,8 @@ class AppApplicationTests {
 
 	@Test
 	void probarEncontrarPaquete() {
-//		String destino = "Paquete Cácota";
-//		Paquete paquete = paqueteService.encontrar(1).get();
-//		log.info(paquete.getNombre());
-		assertEquals(2,2);
+//		assertEquals(2,2);
+		assertEquals(paqueteService.encontrar(1), paqueteService.encontrar(1));
 	}
 
 
