b9457cbfa770cc86fbf93b31c3b31b4672f89c0d
Santiago Serrano <santiagoandressp@ufps.edu.co>
2021-12-08 23:10:21 -0500

    Update CompraRest.java

--- src/main/java/nexp/com/app/rest/CompraRest.java
+++ src/main/java/nexp/com/app/rest/CompraRest.java
@@ -250,20 +250,20 @@ public class CompraRest {
 
     //cantidad de tours vendidos(compras de cualquier tour?)
     // en el mes exceptuando los que devolucion != null
-    @GetMapping(path = "/cantidadToursVendidos")
-    public ResponseEntity<?> cantidadTours(){
-        int cantidadTours = 0;
-        List<Compra> compras = compraservice.listar();
-        Date fechaActual = new Date();
-        for (Compra c: compras){
-            if( c.devolucionCollection().size() == 0 && c.getEstado().equals("PAGADO") &&
-                    c.getFecha().getMonth() == fechaActual.getMonth() &&
-                    c.getFecha().getYear() == fechaActual.getYear()){
-                cantidadTours++;
-            }
-        }
-        return ResponseEntity.ok(cantidadTours);
-    }
+//    @GetMapping(path = "/cantidadToursVendidos")
+//    public ResponseEntity<?> cantidadTours(){
+//        int cantidadTours = 0;
+//        List<Compra> compras = compraservice.listar();
+//        Date fechaActual = new Date();
+//        for (Compra c: compras){
+//            if( c.devolucionCollection().size() == 0 && c.getEstado().equals("PAGADO") &&
+//                    c.getFecha().getMonth() == fechaActual.getMonth() &&
+//                    c.getFecha().getYear() == fechaActual.getYear()){
+//                cantidadTours++;
+//            }
+//        }
+//        return ResponseEntity.ok(cantidadTours);
+//    }
 
     @GetMapping(path = "/totalMeses")
     public ResponseEntity<?> totalMes() throws ParseException {
@@ -387,6 +387,6 @@ public class CompraRest {
 
         return ResponseEntity.ok(total);
     }
-    
+
 
 }
